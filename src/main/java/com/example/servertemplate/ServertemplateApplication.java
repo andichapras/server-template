fcb11d1b563f194c73ea2cc5c68b75906d2e0895
andicha pras <andicha.prasetya@nds.co.id>
2024-08-08 13:36:15 +0700

    init maven project

---
+++ src/main/java/com/example/servertemplate/ServertemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.servertemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServertemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServertemplateApplication.class, args);
+	}
+
+}
